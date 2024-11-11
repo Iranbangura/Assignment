a03dc61f5d03e5323b4171543437bf6196374b38
Iranbangura <111987608+Iranbangura@users.noreply.github.com>
2024-11-11 11:46:05 +0000

    Assignmen Done

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Asking for user input
+name = input("Isatu Bangura ")
+age = input("27")
+location = input("27G Sumaila town Freetown")
+
+# Displaying the personalized message
+print(f"Hi {name}, I am  {age} years old, live in {location}.")
